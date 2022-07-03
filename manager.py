aae892f53eb1a1e4f9d8eb4d239d93a6257868f4
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-07-04 00:36:07 +0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -45,30 +45,30 @@ def clr():
 while True:
     clr()
     banner()
-    print(lg+'[1] Add new accounts'+n)
-    print(lg+'[2] Filter all banned accounts'+n)
-    print(lg+'[3] Delete specific accounts'+n)
-    print(lg+'[4] Update your Astra'+n)
-    print(lg+'[5] Quit'+n)
-    a = int(input('\nEnter your choice: '))
+    print(lg+'[1] Yeni hesap ekle'+n)
+    print(lg+'[2] Tüm yasaklı hesapları filtrele'+n)
+    print(lg+'[3] Belirli hesapları sil'+n)
+    print(lg+'[4] Astra'nızı güncelleyin'+n)
+    print(lg+'[5] Çıkış'+n)
+    a = int(input('\nSeçimini gir: '))
     if a == 1:
         new_accs = []
         with open('vars.txt', 'ab') as g:
-            number_to_add = int(input(f'\n{lg} [~] Enter number of accounts to add: {r}'))
+            number_to_add = int(input(f'\n{lg} [~] Eklenecek hesap sayısını girin: {r}'))
             for i in range(number_to_add):
-                phone_number = str(input(f'\n{lg} [~] Enter Phone Number: {r}'))
+                phone_number = str(input(f'\n{lg} [~] Telefon Numarasını Girin: {r}'))
                 parsed_number = ''.join(phone_number.split())
                 pickle.dump([parsed_number], g)
                 new_accs.append(parsed_number)
-            print(f'\n{lg} [i] Saved all accounts in vars.txt')
+            print(f'\n{lg} [i] Tüm hesapları şuraya kaydetti: vars.txt')
             clr()
             print(f'\n{lg} [*] Logging in from new accounts\n')
             for number in new_accs:
                 c = TelegramClient(f'sessions/{number}', 3910389 , '86f861352f0ab76a251866059a6adbd6')
                 c.start(number)
-                print(f'{lg}[+] Login successful')
+                print(f'{lg}[+] Giriş başarılı')
                 c.disconnect()
-            input(f'\n Press enter to goto main menu...')
+            input(f'\nAna menüye gitmek için enter'a basın...')
 
         g.close()
     elif a == 2:
@@ -82,7 +82,7 @@ while True:
                 break
         h.close()
         if len(accounts) == 0:
-            print(r+'[!] There are no accounts! Please add some and retry')
+            print(r+'[!] Hesap yok! Lütfen biraz ekleyin ve tekrar deneyin')
             sleep(3)
         else:
             for account in accounts:
@@ -92,14 +92,14 @@ while True:
                 if not client.is_user_authorized():
                     try:
                         client.send_code_request(phone)
-                        #client.sign_in(phone, input('[+] Enter the code: '))
+                        #client.sign_in(phone, input('[+] kodu giriniz: '))
                         print(f'{lg}[+] {phone} is not banned{n}')
                     except PhoneNumberBannedError:
                         print(r+str(phone) + ' is banned!'+n)
                         banned_accs.append(account)
             if len(banned_accs) == 0:
-                print(lg+'Congrats! No banned accounts')
-                input('\nPress enter to goto main menu...')
+                print(lg+'Tebrikler! Yasaklı hesap yok')
+                input('\nAna menüye gitmek için enter'a basın...')
             else:
                 for m in banned_accs:
                     accounts.remove(m)
@@ -108,8 +108,8 @@ while True:
                         Phone = a[0]
                         pickle.dump([Phone], k)
                 k.close()
-                print(lg+'[i] All banned accounts removed'+n)
-                input('\nPress enter to goto main menu...')
+                print(lg+'[i] Engellenen tüm hesaplar kaldırıldı'+n)
+                input('\nAna menüye gitmek için enter'a basın...')
 
     elif a == 3:
         accs = []
@@ -121,11 +121,11 @@ while True:
                 break
         f.close()
         i = 0
-        print(f'{lg}[i] Choose an account to delete\n')
+        print(f'{lg}[i] Silmek için bir hesap seçin\n')
         for acc in accs:
             print(f'{lg}[{i}] {acc[0]}{n}')
             i += 1
-        index = int(input(f'\n{lg}[+] Enter a choice: {n}'))
+        index = int(input(f'\n{lg}[+] Bir seçim girin: {n}'))
         phone = str(accs[index][0])
         session_file = phone + '.session'
         if os.name == 'nt':
@@ -136,23 +136,23 @@ while True:
         f = open('vars.txt', 'wb')
         for account in accs:
             pickle.dump(account, f)
-        print(f'\n{lg}[+] Account Deleted{n}')
-        input(f'\nPress enter to goto main menu...')
+        print(f'\n{lg}[+] Hesap Silindi{n}')
+        input(f'\nAna menüye gitmek için enter'a basın...')
         f.close()
     elif a == 4:
-        # thanks to github.com/th3unkn0n for the snippet below
-        print(f'\n{lg}[i] Checking for updates...')
+        # aşağıdaki snippet için github.com/th3unkn0n adresine teşekkürler
+        print(f'\n{lg}[i] Güncellemeleri kontrol etme...')
         try:
             # https://raw.githubusercontent.com/Cryptonian007/Astra/main/version.txt
             version = requests.get('https://raw.githubusercontent.com/Cryptonian007/Astra/main/version.txt')
         except:
-            print(f'{r} You are not connected to the internet')
-            print(f'{r} Please connect to the internet and retry')
+            print(f'{r} İnternete bağlı değilsin')
+            print(f'{r} Lütfen internete bağlanın ve tekrar deneyin')
             exit()
         if float(version.text) > 1.1:
             prompt = str(input(f'{lg}[~] Update available[Version {version.text}]. Download?[y/n]: {r}'))
             if prompt == 'y' or prompt == 'yes' or prompt == 'Y':
-                print(f'{lg}[i] Downloading updates...')
+                print(f'{lg}[i] Güncellemeler indiriliyor...')
                 if os.name == 'nt':
                     os.system('del add.py')
                     os.system('del manager.py')
@@ -163,14 +163,14 @@ while True:
                 os.system('curl -l -O https://raw.githubusercontent.com/Cryptonian007/Astra/main/add.py')
                 os.system('curl -l -O https://raw.githubusercontent.com/Cryptonian007/Astra/main/manager.py')
                 print(f'{lg}[*] Updated to version: {version.text}')
-                input('Press enter to exit...')
+                input('Çıkmak için enter'a basın...')
                 exit()
             else:
                 print(f'{lg}[!] Update aborted.')
-                input('Press enter to goto main menu...')
+                input('Ana menüye gitmek için enter'a basın...')
         else:
-            print(f'{lg}[i] Your Astra is already up to date')
-            input('Press enter to goto main menu...')
+            print(f'{lg}[i] Astra'nız zaten güncel')
+            input('Ana menüye gitmek için enter'a basın...')
     elif a == 5:
         clr()
         banner()
